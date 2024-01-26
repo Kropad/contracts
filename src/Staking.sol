1c14f4a9558c7725f1ae1c85c035ae5f6475faea
Berzan <berzan@tutanota.com>
2024-01-26 08:21:06 +0000

    impl: simple layout

--- src/Staking.sol
+++ src/Staking.sol
@@ -1,44 +1,48 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.13;
 
-contract Launchpad {
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+contract Staking {
     /*//////////////////////////////////////////////////////////////
                                  STRUCTS
     //////////////////////////////////////////////////////////////*/
 
-    struct Project {
-        string name;
-        string description;
-        address token;
-        string site;
-        string dc;
-        string x;
+    struct Stake {
+        uint256 lastStakePoints;
+        uint256 lastStakeTime;
+        uint256 stakeAmount;
     }
 
     /*//////////////////////////////////////////////////////////////
                                 STORAGE
     //////////////////////////////////////////////////////////////*/
 
-    address private immutable token;
-    Project[] public projects;
+    IERC20 private immutable token;
+
+    mapping(address => Stake) getStake;
 
     /*//////////////////////////////////////////////////////////////
                                CONSTRUCTOR
     //////////////////////////////////////////////////////////////*/
 
     constructor(address _token) {
-        token = _token;
+        token = ERC20(_token);
     }
 
     /*//////////////////////////////////////////////////////////////
                                  LOGIC
     //////////////////////////////////////////////////////////////*/
 
-    function list() public {
-        // list logic
+    function stake(uint256 amount) public {
+        // stake logic
     }
 
-    function join() public {
-        // join logic
+    function unstake(uint256 amount) public {
+        // unstake logic
+    }
+
+    function getPoints(uint256 amount) public {
+        // get points logic
     }
 }
--- src/Token.sol
+++ src/Token.sol
@@ -4,8 +4,12 @@ pragma solidity ^0.8.13;
 import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
+    /*//////////////////////////////////////////////////////////////
+                               CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+
     constructor() ERC20("TestToken", "TEST") {
-        uint256 supply = 1_000_000_000;
+        uint256 supply = 1_000_000 * 10 ** decimals();
         _mint(msg.sender, supply);
     }
 }
