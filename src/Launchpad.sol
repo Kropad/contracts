4b9accb5453a58623daf7f530d9aca4f82bb5b82
Berzan <berzan@tutanota.com>
2024-02-04 20:41:10 +0000

    impl: Launchpad contract

--- src/Launchpad.sol
+++ src/Launchpad.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.13;
 
+import {Staking} from "./Staking.sol";
+import {Token} from "./Token.sol";
+
 contract Launchpad {
     /*//////////////////////////////////////////////////////////////
                                  STRUCTS
@@ -8,37 +11,89 @@ contract Launchpad {
 
     struct Project {
         string name;
-        string description;
-        address token;
+        string desc;
         string site;
         string dc;
+        string tg;
         string x;
+        address token;
+        uint256 amount;
+        uint256 price;
     }
 
     /*//////////////////////////////////////////////////////////////
                                 STORAGE
     //////////////////////////////////////////////////////////////*/
 
-    address private immutable token;
+    Staking private immutable staking;
+    Token private immutable token;
     Project[] public projects;
+    uint256 public entryFee;
+
+    mapping(address => mapping(uint256 => bool)) isJoined;
 
     /*//////////////////////////////////////////////////////////////
                                CONSTRUCTOR
     //////////////////////////////////////////////////////////////*/
 
-    constructor(address _token) {
-        token = _token;
+    constructor(address _staking, address _token, uint256 _entryFee) {
+        staking = Staking(_staking);
+        token = Token(_token);
+        entryFee = _entryFee;
     }
 
     /*//////////////////////////////////////////////////////////////
                                  LOGIC
     //////////////////////////////////////////////////////////////*/
 
-    function list() public {
-        // list logic
+    function getProjects() external view returns (Project[] memory) {
+        return projects;
     }
 
-    function join() public {
-        // join logic
+    function hasJoined(
+        address _addr,
+        uint256 _projectIndex
+    ) external view returns (bool) {
+        return isJoined[_addr][_projectIndex];
+    }
+
+    function listProject(
+        string memory _name,
+        string memory _desc,
+        string memory _site,
+        string memory _dc,
+        string memory _tg,
+        string memory _x,
+        address _token,
+        uint256 _amount,
+        uint256 _price
+    ) external {
+        Project storage project;
+
+        project.name = _name;
+        project.desc = _desc;
+        project.site = _site;
+        project.dc = _dc;
+        project.tg = _tg;
+        project.x = _x;
+        project.token = _token;
+        project.amount = _amount;
+        project.price = _price;
+
+        // todo: list project logic
+
+        projects.push(project);
+    }
+
+    function joinProject(uint256 _projectIndex) public {
+        Project storage project = projects[_projectIndex];
+        uint256 points = staking.calculatePoints(msg.sender);
+        bool isAlreadyJoined = isJoined[msg.sender][_projectIndex];
+
+        require(!isAlreadyJoined, "already joined this project");
+
+        // todo: join project logic
+
+        isJoined[msg.sender][_projectIndex] = true;
     }
 }
